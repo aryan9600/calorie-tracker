17eedf094e6eb1941071bb7dd0feaabbc53c12e8
Sanskar Jaiswal <jaiswalsanskar078@gmail.com>
2023-12-10 18:40:24 +0530

    update postinstall script
    Signed-off-by: Sanskar Jaiswal <jaiswalsanskar078@gmail.com>

--- update-stellar-sdk.sh
+++ update-stellar-sdk.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+if [ ! ./bin/soroban ]; then
+    os=$(uname -s)
+    if [[ "$os" == "Darwin" ]]; then
+        wget -O soroban-cli.tar.gz https://github.com/stellar/soroban-tools/releases/download/v20.0.1/soroban-cli-20.0.1-aarch64-apple-darwin.tar.gz
+    else
+        wget -O soroban-cli.tar.gz https://github.com/stellar/soroban-tools/releases/download/v20.0.1/soroban-cli-20.0.1-x86_64-unknown-linux-gnu.tar.gz
+    fi
+    tar -xf soroban-cli.tar.gz && mv soroban ./bin
+    rm soroban-cli.tar.gz
+fi
 
-soroban contract bindings typescript \
+./bin/soroban contract bindings typescript \
     --network futurenet \
     --contract-id $(cat .soroban/calorie-tracker-id) \
     --output-dir node_modules/calorie-tracker-client
